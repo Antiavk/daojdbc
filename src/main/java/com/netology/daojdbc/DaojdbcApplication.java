ce54a0ebff6f08b415ad21da5899af18d213aeb8
Antiavk <avk_tp@mail.ru>
2023-04-24 20:04:56 +0300

    Initial commit

---
+++ src/main/java/com/netology/daojdbc/DaojdbcApplication.java
@@ -0,0 +1,13 @@
+package com.netology.daojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaojdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaojdbcApplication.class, args);
+    }
+
+}
